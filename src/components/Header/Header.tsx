e8d7faab916d5ffe8980b8a817fe0091a63df47a
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-05-03 13:29:49 +0300

    feat: add base layout with Header and Main class components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="header__title">Poke Search</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <main className="main">
+        <p className="main__placeholder">Results will appear here</p>
+      </main>
+    );
+  }
+}
+
+export default Main;
