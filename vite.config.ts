42f927158344df85bd6d9da25e0878867084aed9
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-06-06 16:20:11 +0300

    chore: replace tailwind with scss modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [fileURLToPath(new URL('./src', import.meta.url))],
+      },
     },
   },
   test: {
